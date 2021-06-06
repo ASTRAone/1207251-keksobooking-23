d0f2c9e51f6ff9b1e31ad7015c0a54aea7989335
Vladislav <vladway827@gmail.com>
2021-06-06 17:09:00 +0300

    update version

--- js/main.js
+++ js/main.js
@@ -1,3 +1,23 @@
+// Рандомное число
+const randomNumber = (min, max) => {
+    if (min >= 0 && max >= 0) {
+      const num = Math.floor(Math.random() * (max - min + 1) + min);
+  
+      return num < 0 ? randomNumber(min, max) : num;
+    }
+    return 0;
+  };
+  
+  // Рандомное число с плавающей точкой
+  const randomFloatNumber = (min, max, point) => {
+    if (min >= 0 || max >= 0) {
+      const num = parseFloat((Math.random() * (max - min) + min).toFixed(point));
+  
+      return num < 0 ? randomFloatNumber(min, max, point) : num;
+    }
+    return 0;
+  };
+
 const fillingArrObj = (arr = []) => {
 
   const OFFER_FEATURES = [
